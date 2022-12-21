76225e99893454d4c2a0bde132b6dda18689bded
Vasiliy <vasenka_kuznetsov_1997@mail.ru>
2022-12-21 22:18:42 +0300

    Fix_Code

--- src/main/java/ru/netology/sqr/DZ5/services/SQRService.java
+++ src/main/java/ru/netology/sqr/DZ5/services/SQRService.java
@@ -3,8 +3,8 @@ package ru.netology.sqr.DZ5.services;
 public class SQRService {
     public int sqrtCeil(int number1, int number2) {
         int count = 0;
-        for (int i = 10; i <=99; i++) {
-            if ((i*i >= number1) && (i*i <= number2)) {
+        for (int i = 10; i <= 99; i++) {
+            if ((i * i >= number1) && (i * i <= number2)) {
                 count++;
             }
         }
--- src/test/java/ru/netology/sqr/DZ5/services/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/DZ5/services/SQRServiceTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
-
 public class SQRServiceTest {
     @Test
     public void NumberOfSquares1() {
