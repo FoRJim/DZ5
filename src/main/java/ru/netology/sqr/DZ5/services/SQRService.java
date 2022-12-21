49f7e48cb641b6c871cec7b7c984daa9e74902a8
Vasiliy <vasenka_kuznetsov_1997@mail.ru>
2022-12-21 22:14:19 +0300

    Initial commit

---
+++ src/main/java/ru/netology/sqr/DZ5/services/SQRService.java
@@ -0,0 +1,13 @@
+package ru.netology.sqr.DZ5.services;
+
+public class SQRService {
+    public int sqrtCeil(int number1, int number2) {
+        int count = 0;
+        for (int i = 10; i <=99; i++) {
+            if ((i*i >= number1) && (i*i <= number2)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/ru/netology/sqr/DZ5/services/SQRServiceTest.java
@@ -0,0 +1,17 @@
+package ru.netology.sqr.DZ5.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+public class SQRServiceTest {
+    @Test
+    public void NumberOfSquares1() {
+        SQRService service = new SQRService();
+        int expected = 11;
+        int actual = service.sqrtCeil(100, 400);
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
